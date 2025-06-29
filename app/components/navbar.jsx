5d3ac4fab16d2d1c4eea77f56cb45fcc1bf2b391
natyavidhan <natyavidhanbiswas10@gmail.com>
2025-06-29 11:16:52 +0530

    navbar reformat

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
 
-import logo from "../assets/Ui/ArvieKGamesLogoFull.png";
+import logo from "../assets/Ui/Arvie_K_Games_Nameplate.png";
 
 // Updated socials array structure
 const socials = [
-  { url: "https://www.instagram.com/arvie_k/", icon: "/icons/instagram.png", alt: "Instagram" },
   { url: "https://www.youtube.com/@ArvieKGames", icon: "/icons/youtube.png", alt: "YouTube" },
   { url: "https://arviek.itch.io/", icon: "/icons/itch.png", alt: "Spotify" },
-  { url: "https://discord.gg/tJcNcxsREP", icon: "/icons/discord.png", alt: "TikTok" }
+  { url: "https://discord.gg/tJcNcxsREP", icon: "/icons/discord.png", alt: "TikTok" },
+  { url: "https://www.instagram.com/arvie_k/", icon: "/icons/instagram.png", alt: "Instagram" },
 ];
 
 
